c0790d8ae41a0bbcdfaf7d1679c0f4ff0a44f541
peerpressurre <gsophiay@gmail.com>
2023-04-01 23:54:27 +0300

    Добавьте файлы проекта.

---
+++ library.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+using std::cout;
+using std::istream;
+using std::ostream;
+using std::fstream;
+using std::ios_base;
+#define next_line { cout << endl; }
